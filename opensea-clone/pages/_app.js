7336f6cb972f41d9a63e8f4225343d28d2e5a841
MBusto <mikebusto4@gmail.com>
2022-02-02 15:02:33 -0500

    Feat: Finished implementing connecting wallet with MetaMask

--- opensea-clone/pages/_app.js
+++ opensea-clone/pages/_app.js
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
-import { AppProps } from 'next/app'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+/* 
+  * Chain ID 4 represents the Rinkeby network
+  * The injected connector is a web3 connection method used by Metamask
+*/
+const supportedChainIds = [4]
+const connectors = {
+  injected: {}
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    //Wrapping the whole app in the ThirdwebWeb3Provider
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}>
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
+
 }
 
 export default MyApp
